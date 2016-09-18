d38832769a1342f8cbc8eba9f04811c66e58af5d
Mr Thanh Phong <tranthanhphongcntt2009@gmail.com>
2016-09-18 21:48:17 +0700

    Add Web sErvicd

---
+++ Web Service/include/Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'u190393894_and');
+define('DB_PASSWORD', 'tranthanhphong1989');
+define('DB_HOST', 'mysql.hostinger.vn');
+define('DB_NAME', 'u190393894_and');
+
+
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+?>
